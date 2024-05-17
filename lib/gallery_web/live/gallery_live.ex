58886fbc1321e866d235f198b25430d4be0281f5
surrsurus <surrsurus@protonmail.com>
2024-05-16 23:54:53 -0400

    Doc

--- lib/gallery_web/live/gallery_live.ex
+++ lib/gallery_web/live/gallery_live.ex
@@ -1,16 +1,20 @@
 defmodule GalleryWeb.GalleryLive do
+  @moduledoc """
+  The entrypoint to the gallery
+
+  This just sends out an event to cause the browser to attach the three JS canvas to the web page.
+  This could have been a controller or something but I prefer the hook syntax, just because I dislike hiding
+  script tags in the heex. This makes it more clear what's happening as you look through the JS.
+  """
   use GalleryWeb, :live_view
 
   require Logger
 
   def mount(_params, _session, socket) do
-    socket =
-      if connected?(socket) do
-        push_event(socket, "start_scene", %{})
-      else
-        socket
-      end
-
-    {:ok, socket}
+    if connected?(socket) do
+      {:ok, push_event(socket, "start_scene", %{})}
+    else
+      {:ok, socket}
+    end
   end
 end
