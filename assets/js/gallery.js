388b053fb34456f9d1566499493ea76c50683362
surrsurus <surrsurus@protonmail.com>
2024-05-18 01:24:17 -0400

    Better camera, add obvious superclass

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,5 +1,5 @@
 import { channel } from "./gallery_socket.js"
-import { animate, addCanvas, fromPhoenix } from './main.js';
+import { animate, addCanvas, resetCamera, Player} from './main.js';
 
 // store player id
 let my_id = null;
@@ -9,20 +9,32 @@ let current_players = {};
 
 // only start up the three.js scene if we get the sign we're all connected from the phoenix live page
 window.addEventListener("phx:start_scene", (_e) => {
-  const canvas = addCanvas();
-  animate();
+  prepareCanvas();
 
   // on join we get our id, and current players
   channel.join()
     .receive("ok", ({ players: players, id: id }) => {
       // we render the players, then tell the server we're ready
       // this will cause the server to send out messages letting everyone else know we can be rendered
-      players.map(player_data => current_players[player_data.id] = fromPhoenix(player_data));
+      players.map(addNewPlayer);
       my_id = id;
 
       channel.push("ready", { id: id });
     })
     .receive("error", resp => { console.log("Unable to join", resp) });
+});
+
+// new player ready, start rendering them
+channel.on("player_joined", ({ player: player_data }) => addNewPlayer(player_data));
+
+// player left, stop rendering them
+channel.on("player_left", ({ id: id }) => current_players[id].removeFromScene());
+
+// player position changed, update them
+channel.on("player_moved", ({ id: id, dx: dx, dy: dy, dz: dz }) => current_players[id].updatePosition(dx, dy, dz));
+
+function prepareCanvas() {
+  const canvas = addCanvas();
 
   // let canvas be focusable so the event listener can catch key events
   canvas.tabIndex = 1;
@@ -46,17 +58,18 @@ window.addEventListener("phx:start_scene", (_e) => {
       case 'D':
         channel.push("update_position", { id: my_id, dx: 1, dy: 0, dz: 0 });
         break;
+      case 'r':
+      case 'R':
+        resetCamera();
+        break;
       default:
         return;
     }
   });
-});
 
-// new player ready, start rendering them
-channel.on("player_joined", ({ player: player_data }) => current_players[player_data.id] = fromPhoenix(player_data));
+  animate();
+}
 
-// player left, stop rendering them
-channel.on("player_left", ({ id: id }) => current_players[id].removeFromScene());
-
-// player position changed, update them
-channel.on("player_moved", ({ id: id, dx: dx, dy: dy, dz: dz }) => current_players[id].updatePosition(dx, dy, dz));
+function addNewPlayer(player_data) {
+  current_players[player_data.id] = Player.fromPhoenix(player_data);
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,56 +3,114 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // Initialize Three.js scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const controls = new OrbitControls(camera, renderer.domElement);
-renderer.setSize(window.innerWidth, window.innerHeight);
+const renderer = createRenderer();
+const camera = createCamera();
+const controls = createControls(camera);
 
-camera.position.set( 0, 1, 5 );
-controls.update();
+class Drawable {
+  constructor(drawable, position, autoAdd = true) {
+    this.drawable = drawable;
+    this.drawable.position.copy(position)
 
-class Player {
-  constructor(geometry, material, position) {
-    this.geometry = geometry;
-    this.material = material;
-
-    this.player = new THREE.Mesh(geometry, material);
-    this.player.position.set(position.x, position.y, position.z);
+    if (autoAdd) {
+      this.addToScene();
+    }
   }
 
   addToScene() {
-    scene.add(this.player);
+    scene.add(this.drawable)
   }
 
   removeFromScene() {
-    scene.remove(this.player);
+    scene.remove(this.drawable);
   }
 
   updatePosition(dx, dy, dz) {
-    this.player.position.set(this.player.position.x + dx, this.player.position.y + dy, this.player.position.z + dz);
+    const delta_pos = new THREE.Vector3(dx, dy, dz)
+    this.drawable.position.add(delta_pos);
   }
 }
 
-export function fromPhoenix(player) {
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: player.color });
-  const position = new THREE.Vector3(player.x, player.y, player.z);
-  const p = new Player(geometry, material, position);
+export class Player extends Drawable {
+  constructor(geometry, material, position) {
+    const player = new THREE.Mesh(geometry, material);
+    super(player, position)
+  }
 
-  p.addToScene();
+  static fromPhoenix(player) {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshLambertMaterial({ color: player.color });
+    const position = new THREE.Vector3(player.x, player.y, player.z);
 
-  return p;
+    return new Player(geometry, material, position);
+  }
 }
+
+export class Light extends Drawable {
+  constructor(position, color = 0xffffff, intensity = 1, distance = 0) {
+    const light = new THREE.PointLight(color, intensity, distance);
+    super(light, position)
+  }
+}
+
+const mainLight = new Light(new THREE.Vector3(10, 10, 10));
 
 // Add the canvas to the current page
 export function addCanvas() {
   document.body.appendChild(renderer.domElement);
+
   return document.querySelectorAll('[data-engine="three.js r164"]')[0];
 }
 
 // Animate the scene
 export function animate() {
-  requestAnimationFrame(animate);
   controls.update();
+
+  // grow light in intensity when first loading
+  // TODO: would be cool if i could do this and not constatly 
+  // check this as part of my animation loop after it's finished
+  if (mainLight.drawable.intensity < 250) {
+    mainLight.drawable.intensity += 1
+    console.log(mainLight.drawable.intensity)
+  }
+
   renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
+
+export function resetCamera() {
+  // if you reset in the middle of a sweep, the camera 
+  // will still move after the reset due to damping
+  // this feels weird, so this little hack disables 
+  // damping to stop that motion, then renables it after the reset
+  controls.enableDamping = false;
+  controls.update();
+  controls.reset();
+  controls.update();
+  controls.enableDamping = true;
+  controls.update();
+}
+
+function createCamera() {
+  const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, 5);
+
+  return camera;
+}
+
+function createControls(camera) {
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.update();
+  controls.saveState();
+
+  return controls;
+}
+
+function createRenderer() {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  return renderer;
 }
