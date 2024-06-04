71adb56a9787175d5a1ae7f725d54d477439a1a8
surrsurus <surrsurus@protonmail.com>
2024-06-04 19:22:46 -0400

    Better specs + lint

--- lib/gallery_web/channels/gallery_channel.ex
+++ lib/gallery_web/channels/gallery_channel.ex
@@ -6,22 +6,23 @@ defmodule GalleryWeb.GalleryChannel do
 
   ## Incoming Messages
 
-  `join` - Joining this channel registers you in the player cache.
-           If the join is successful, you'll be given the list of current players and your current player in the payload
+  `join` - Joining this channel registers you in the player cache. If the join is successful,
+           you'll be given the list of current players and your current player in the payload
 
 
-  `ready` - Signal to the server that you have received the player data from joining. All other players are given your player payload
-            to begin rendering you on their clients. Fires `player_joined` back to the clients.
+  `ready` - Signal to the server that you have received the player data from joining. All other players
+            are given your player payload to begin rendering you on their clients. Fires `player_joined` back to the clients.
 
-  `update_position` - Signal to the server that you have changed your position and/or rotation. All other players are given your
-                      updated player payload. Fires `player_moved` back to the client.
-
+  `update_position` - Signal to the server that you have changed your position and/or rotation. All other
+                      players are given your updated player payload. Fires `player_moved` back to the client.
 
   ## Outgoing Messages
 
-  `player_joined` - Fired when a new player joins the channel. You will not receive your `player_joined` events you trigger.
+  `player_joined` - Fired when a new player joins the channel.
+                    You will not receive your `player_joined` events you trigger.
 
-  `player_moved` - Fired when a player updates their position and/or rotation. You will not receive `player_moved` events you trigger.
+  `player_moved` - Fired when a player updates their position and/or rotation.
+                   You will not receive `player_moved` events you trigger.
 
   `player_left` - On disconnect, all players will be notified of your departure so you can stop being rendered on their clients.
   """
@@ -36,8 +37,12 @@ defmodule GalleryWeb.GalleryChannel do
 
   @room "gallery:main"
 
-  @spec join(String.t(), map(), Phoenix.Socket.t()) ::
-          {:ok, %{players: [Player.t()], you: Player.t()}, Phoenix.Socket.t()}
+  @type gallery_socket :: %Phoenix.Socket{assigns: %{player_id: String.t()}}
+  @type join_response :: %{players: [Player.t()], you: Player.t()}
+  @type ready_payload :: %{required(:id) => String.t()}
+  @type update_payload :: %{required(:pos) => map(), required(:rot) => map()}
+
+  @spec join(String.t(), map(), gallery_socket()) :: {:ok, join_response(), gallery_socket()}
   def join(@room, %{}, %{assigns: %{player_id: player_id}} = socket) do
     existing_players = PlayerCache.all()
     incoming_player = Player.new!(player_id)
@@ -46,8 +51,7 @@ defmodule GalleryWeb.GalleryChannel do
     {:ok, %{players: existing_players, you: incoming_player}, socket}
   end
 
-  @spec handle_in(String.t(), %{required(:id) => String.t()}, Phoenix.Socket.t()) ::
-          {:noreply, Phoenix.Socket.t()}
+  @spec handle_in(String.t(), ready_payload(), gallery_socket()) :: {:noreply, gallery_socket()}
   def handle_in("ready", %{"id" => player_id}, %{assigns: %{player_id: player_id}} = socket) do
     player = PlayerCache.get(player_id)
 
@@ -55,11 +59,7 @@ defmodule GalleryWeb.GalleryChannel do
     {:noreply, socket}
   end
 
-  @spec handle_in(
-          String.t(),
-          %{required(:pos) => map(), required(:rot) => map()},
-          Phoenix.Socket.t()
-        ) :: {:noreply, Phoenix.Socket.t()}
+  @spec handle_in(String.t(), update_payload(), gallery_socket()) :: {:noreply, gallery_socket()}
   def handle_in(
         "update_position",
         %{"pos" => pos, "rot" => rot} = payload,
@@ -73,7 +73,7 @@ defmodule GalleryWeb.GalleryChannel do
     {:noreply, socket}
   end
 
-  @spec handle_in(String.t(), map(), Phoenix.Socket.t()) :: {:noreply, Phoenix.Socket.t()}
+  @spec handle_in(String.t(), map(), gallery_socket()) :: {:noreply, gallery_socket()}
   def handle_in(event, payload, %{assigns: %{player_id: player_id}} = socket) do
     Logger.debug(
       "[GalleryChannel] Player #{player_id} sent to #{event} an unknown response: #{inspect(payload)}"
@@ -82,35 +82,28 @@ defmodule GalleryWeb.GalleryChannel do
     {:noreply, socket}
   end
 
-  def handle_out(
-        "player_joined",
-        %{"player" => player} = payload,
-        %{assigns: %{player_id: player_id}} = socket
-      )
-      when player.id != player_id do
+  @spec handle_out(String.t(), map(), gallery_socket()) :: {:noreply, gallery_socket()}
+  def handle_out("player_joined", %{"player" => player}, %{assigns: %{player_id: player_id}} = socket)
+      when player.id == player_id do
+    {:noreply, socket}
+  end
+
+  def handle_out("player_joined", payload, socket) do
     push(socket, "player_joined", payload)
     {:noreply, socket}
   end
 
-  def handle_out("player_joined", _payload, socket) do
+  def handle_out("player_moved", %{"id" => id}, %{assigns: %{player_id: player_id}} = socket)
+      when id == player_id do
     {:noreply, socket}
   end
 
-  def handle_out(
-        "player_moved",
-        %{"id" => id} = payload,
-        %{assigns: %{player_id: player_id}} = socket
-      )
-      when id != player_id do
+  def handle_out("player_moved", payload, socket) do
     push(socket, "player_moved", payload)
     {:noreply, socket}
   end
 
-  def handle_out("player_moved", _payload, socket) do
-    {:noreply, socket}
-  end
-
-  @spec terminate(any(), Phoenix.Socket.t()) :: Phoenix.Socket.t()
+  @spec terminate(any(), gallery_socket()) :: gallery_socket()
   def terminate(_reason, %{assigns: %{player_id: player_id}} = socket) do
     PlayerCache.remove(player_id)
 
